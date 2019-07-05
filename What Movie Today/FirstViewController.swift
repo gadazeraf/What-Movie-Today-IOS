8254826cddd06cc06831929ff21e8ccc9b756a38
Gad Azeraf <gad.azeraf@epita.fr>
2019-07-05 18:25:15 +0200

    Initial Commit

---
+++ What Movie Today/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  What Movie Today
+//
+//  Created by Gad on 05/07/2019.
+//  Copyright © 2019 Azeraf. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ What Movie Today/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  What Movie Today
+//
+//  Created by Gad on 05/07/2019.
+//  Copyright © 2019 Azeraf. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
